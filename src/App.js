bc714fd553aec51e3ae52b6bb0ffc01d3e281605
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-26 19:05:32 -0600

    adding the basic colors and the dark mode also

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {ColorModeContext, useMode} from "./theme"
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { Theme } from '@fullcalendar/core/internal'
 
 
 const App = () => {
+  const [theme, colorMode]= useMode()
   return (
-    <div className='app'>App</div>
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+    <div className='app'>
+      <main className='content'>dfsb</main>
+    </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+
   )
 }
 
