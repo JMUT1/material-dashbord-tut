3c1594062cf4611f813c350f0043d1c3f7c65b5a
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-31 16:07:10 -0600

    create the sidebar with functionalities and also the header component that I use it in the Dashboard component but it is reusable

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from '../../components/Header'
+import { Box } from '@mui/system'
 
 const Dasboard = () => {
   return (
-    <div>Dasboard</div>
+    <Box m="20px"> 
+    <Box display="flex" justifyContent="space-between" alignContent="center">
+      <Header title="DASHBORD" subtitle="Welcome to your dashboard"/>
+    </Box>
+    </Box>
   )
 }
 
